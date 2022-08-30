ef8fc5dbdedf084437e7294b8f06c208f483f051
RISHABH ANAND <94896142+thanosuser@users.noreply.github.com>
2022-08-30 09:05:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,6 +24,8 @@ class Development(Config):
   BOT_USERNAME = "YOUR VALUE"
   # Custom Command Handler. 
   HANDLER = "."
+  #loger I'd
+  LOGGER_ID = "Your value"
 
   # Custom Command Handler for sudo users.
   SUDO_HANDLER = "!"
