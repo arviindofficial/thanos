c8c2d1eabfcc3d98bacba7e12c3a26cb5015541b
RISHABH ANAND <94896142+thanosuser@users.noreply.github.com>
2022-08-30 08:57:42 +0530

    Fixed

--- THANOSPRO/sql/__init__.py
+++ THANOSPRO/sql/__init__.py
@@ -1,14 +1,16 @@
 import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.orm import scoped_session, sessionmaker
 
-# from THANOSPRO.config import Config
+from THANOSPRO.config import Config
 
-DB_URI = os.environ.get("DATABASE_URL")
+# DB_URI = os.environ.get("DATABASE_URL")
+
 
 def start() -> scoped_session:
-    engine = create_engine(DB_URI)
+    engine = create_engine(Config.DB_URI)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
@@ -19,5 +21,7 @@ try:
     SESSION = start()
 except AttributeError as e:
     # this is a dirty way for the work-around required for #23
-    print("DB_URI is not configured. Features depending on the database might have issues.")
+    print(
+        "DB_URI is not configured. Features depending on the database might have issues."
+    )
     print(str(e))
